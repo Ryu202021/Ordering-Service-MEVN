1db6507596ac926b049caf282771e545a02734c7
Ryan <rhui1@bayviewglen.ca>
2021-01-06 15:06:42 -0500

    added login page

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+      <div class=''>
+      <h1>HELLO</h1>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+<style>
+
+</style>
