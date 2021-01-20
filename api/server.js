cfd9c1359c18d32f8e3e721edf33970dbb94040e
Nick Tong <nicktong.001@gmail.com>
2021-01-19 19:22:18 -0500

    Connected app to online database
    Mongo atlas database connected

--- api/server.js
+++ api/server.js
@@ -6,22 +6,27 @@ const bodyParser = require('body-parser');
 const PORT = 6000;
 const cors = require('cors');
 const mongoose = require('mongoose');
-const config = require('./DB.js');
+//const config = require('./DB.js');
 const restaurantRoutes = require('./routes/restaurantRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
-);
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(
+    () => { console.log('Database is connected'); },
+    err => { console.log('Can not connect to the database: ' + err); }
+  );
 
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/orders', orderRoutes);
 app.use('/restaurants', restaurantRoutes);
-app.listen(PORT, function(){
-  console.log('Server is running on Port:',PORT);
+app.listen(PORT, function () {
+  console.log('Server is running on Port:', PORT);
 });
